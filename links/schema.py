fc91c71f9f6e95acfa61398d99471d2ecdb519d8
Sean Bolton <sean@smbolton.com>
2017-11-06 09:44:46 -0800

    Front-end tutorial through 6-more-mutations-and-updating-the-store.
    Reorganize and rewrite comments so links/schema.py reads much better straight through.

--- links/schema.py
+++ links/schema.py
@@ -34,10 +34,26 @@ from users.schema import get_user_from_auth_token, User
 
 # ========== Vote ==========
 
+# There are two common GraphQL schema patterns that are difficult to implement with graphene-django
+# 2.0 without distasteful monkey-patching. These are:
+#
+# - DjangoConnectionField can no longer be given a custom Connection, which makes it difficult to
+#   add custom fields (such as the ubiquitous 'count' or 'totalCount') on the connection, and
+#
+# - django_filters FilterSet can't use custom Enums for OrderingFilter choices.
+#
+# The VoteConnection here demonstrates how to use a custom Connection, with a 'count' field and
+# a non-Enum-using FilterSet, with no monkey patching.
+#
+# LinkConnection below demonstrates a way to use custom Enums in a custom Connection.
+
 class Vote(DjangoObjectType):
     class Meta:
         model = VoteModel
         interfaces = (relay.Node, )
+        # We are going to provide a custom Connection, so we need to tell graphene-django not to
+        # create one. Failing to do this will result in a error like "AssertionError: Found
+        # different types with the same name in the schema: VoteConnection, VoteConnection."
         use_connection = False
 
 
@@ -46,30 +62,41 @@ class IdInput(graphene.InputObjectType):
 
 
 class VoteFilter(graphene.InputObjectType):
+    """The input object for filtered allVotes queries. The VoteFilter input type provided by the
+    Graphcool server used in the front-end tutorial is quite a bit more complex than this, but this
+    is all the tutorial itself needs.
+    """
     link = graphene.InputField(IdInput)
     user = graphene.InputField(IdInput)
 
 
 class VotesFilterSet(django_filters.FilterSet):
+    """A basic FilterSet for filtering allVotes queries."""
     class Meta:
         model = VoteModel
         fields = ['link', 'user']
 
 
 class VoteConnection(relay.Connection):
+    """A custom Connection for queries on Vote, complete with custom field 'count'."""
     class Meta:
         node = Vote
 
     count = graphene.Int()
 
+    # 'allVotes' is actually a field on Viewer, and 'votes' is a field on Link, but rather than put
+    # a bunch of Vote-related logic in Viewer and Link, I prefer to keep it here as static methods
+    # on VoteConnection.
     @staticmethod
     def get_all_votes_input_fields():
+        """Return the input fields needed by allVotes."""
         return {
             'filter': graphene.Argument(VoteFilter),
         }
 
     @staticmethod
     def resolve_all_votes(_, info, filter=None):
+        """Resolve a field returning a (possibly filtered view of) all Votes."""
         qs = VoteModel.objects.all()
         if filter:
             # We don't get the free input marshalling that DjangoFilterConnectionField provides, so
@@ -87,11 +114,13 @@ class VoteConnection(relay.Connection):
         return qs
 
     def resolve_count(self, info, **args):
+        """Return the count of votes in the LinkConnection query."""
         # self.iterable is the QuerySet of VoteModels
         return self.iterable.count()
 
     @staticmethod
     def resolve_votes(parent, info, **args):
+        """Resolve the 'votes' field on Link by returning all votes made by this user."""
         # parent is a LinkModel
         qs = VoteModel.objects.filter(link_id=parent.pk)
         return qs
@@ -146,14 +175,33 @@ class CreateVote(relay.ClientIDMutation):
 
 # ========== Link ==========
 
+# The GraphQL schema used by the front-end tutorial uses GraphQL Enums for specifying the sort order
+# used by its connections, for example:
+#
+#     enum LinkOrderBy {
+#       createdAt_ASC
+#       createdAt_DESC
+#       description_ASC
+#       description_DESC
+#       id_ASC
+#       id_DESC
+#       updatedAt_ASC
+#       updatedAt_DESC
+#       url_ASC
+#       url_DESC
+#     }
+#
+# graphene_django has some provision for generating Enums from choice-containing fields in a
+# DjangoObjectType, but the Enum type name and value names are automatically generated with no way
+# to control them. Furthermore, for the tutorial we need the Enum types for ordering the
+# LinkConnection, and DjangoFilterConnectionType makes no provision for custom enums in FilterSets.
+# So, we're back to using a custom Connection.
+
 class Link(DjangoObjectType):
     class Meta:
         model = LinkModel
         interfaces = (Node, )
-        # We are going to provide a custom Connection, so we need to tell graphene-django not to
-        # create one. Failing to do this will result in a error like "AssertionError: Found
-        # different types with the same name in the schema: LinkConnection, LinkConnection."
-        use_connection = False
+        use_connection = False  # a custom Connection will be provided
 
     votes = relay.ConnectionField(
         VoteConnection,
@@ -163,42 +211,38 @@ class Link(DjangoObjectType):
 
 class LinkOrderBy(graphene.Enum):
     """This provides the schema's LinkOrderBy Enum type, for ordering LinkConnection."""
+    # The class name ('LinkOrderBy') is what the GraphQL schema Enum type name should be, the
+    # left-hand side below is what the Enum values should be, and the right-hand side is what our
+    # resolver will receive.
     createdAt_DESC = '-created_at'
     createdAt_ASC = 'created_at'
     description_ASC = 'description'
     description_DESC = '-description'
     id_ASC = 'id'
     id_DESC = '-id'
-    #updatedAt_ASC = 'updated_at'
-    #updatedAt_DESC = '-updated_at'
+    #updatedAt_ASC = 'updated_at'   -- these are present in the Graphcool schema, but not needed by
+    #updatedAt_DESC = '-updated_at'    the tutorial, nor implemented in LinkModel
     url_ASC = 'url'
     url_DESC = '-url'
 
 
 class LinkConnection(relay.Connection):
-    """A custom Connection for queries on Link. Using this instead of DjangoConnectionField or
-    DjangoFilterConnectionField solves two problems:
-    - django_filters FilterSet can't use custom enums for OrderFilter choices, and
-    - DjangoConnectionField in graphene-django 2.0 can no longer be given a custom Connection,
-        which makes it difficult to add custom fields (such as the ubiquitous 'totalCount') on the
-        connection without monkey-patching.
-    """
-
-    # total_count = graphene.Int()  -- a custom field on the connection, to be added later
-
+    """A custom Connection for queries on Link."""
     class Meta:
         node = Link
 
     @staticmethod
     def get_all_links_input_fields():
         return {
-            'order_by': graphene.Argument(LinkOrderBy) # an input field using the custom enum
+            # this creates an input field using the LinkOrderBy custom enum
+            'order_by': graphene.Argument(LinkOrderBy)
         }
 
-    def resolve_all_links(self, info, **args):
+    def resolve_all_links(self, info, order_by):
         qs = LinkModel.objects.all()
-        order_by = args.get('order_by')
         if order_by:
+            # Graphene has already translated the over-the-wire enum value (e.g. 'createdAt_DESC')
+            # to our internal value ('-created_at') needed by Django.
             qs = qs.order_by(order_by)
         return qs
 
@@ -256,6 +300,14 @@ class CreateLink(relay.ClientIDMutation):
 
 
 # ========== schema structure ==========
+
+# A common question I've seen regarding graphene, and GraphQL back-ends in general, is "what creates
+# this 'viewer' field my front-end is expecting?" Many Relay applications have GraphQL schemas that
+# include a 'viewer' field, but 'viewer' is not part of the GraphQL or Relay specifications.
+# Instead, it is just a common and useful pattern for introducing user authentication and/or
+# grouping top-level queries. Here is a simple viewer implementation, which includes the requisite
+# Relay Node and wraps our top-level queries. Note that there's no Django involved at this level,
+# just graphene routing queries to the appropriate resolvers.
 
 class Viewer(ObjectType):
     class Meta:
